cd36db8218d33be6677aab932827659a589fec93
Killian <killian.hd@gmail.com>
2023-05-16 17:47:13 +0200

    Adaptation in the Network dimension

--- lib/utils/learning.py
+++ lib/utils/learning.py
@@ -80,7 +80,7 @@ def load_backbone(args):
     if not(hasattr(args, "backbone")):
         args.backbone = 'DSTformer' # Default
     if args.backbone=='DSTformer':
-        model_backbone = DSTformer(dim_in=3, dim_out=3, dim_feat=args.dim_feat, dim_rep=args.dim_rep, 
+        model_backbone = DSTformer(dim_in=args.dim_bbox, dim_out=args.dim_bbox, dim_feat=args.dim_feat, dim_rep=args.dim_rep, 
                                    depth=args.depth, num_heads=args.num_heads, mlp_ratio=args.mlp_ratio, norm_layer=partial(nn.LayerNorm, eps=1e-6), 
                                    maxlen=args.maxlen, num_joints=args.num_joints)
     elif args.backbone=='TCN':
